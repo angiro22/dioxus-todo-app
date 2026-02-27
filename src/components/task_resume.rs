96014d3956eeaff27b5cdebb035d9f42ea9ea28a
angiro22 <andreagirone2710@gmail.com>
2026-02-27 01:24:07 +0100

    Refactoring task_resume

---
+++ src/components/task_resume.rs
@@ -0,0 +1,39 @@
+use dioxus::prelude::*;
+
+#[derive(Props, Clone, PartialEq)]
+pub struct TaskProps {
+    title: String,
+    description: Option<String>,
+    done: bool
+}
+
+#[component]
+pub fn TaskResume(props: TaskProps) -> Element {
+    let mut is_done = use_signal(|| props.done);
+    let dynamic_class = if is_done() { "completed" } else { "" };
+
+    rsx! {
+        div { class: "task-card",
+
+            input {
+                class: "checkbox",
+                r#type: "checkbox",
+                checked: is_done,
+
+                oninput: move |_| { is_done.set(!is_done()) }
+            }
+
+            div {
+                class: "task-info {dynamic_class}",
+
+                h3 { class: "task-title", 
+                    "{props.title}" 
+                }
+
+                        if let Some(desc) = props.description {
+                p { class: "task-description {dynamic_class}", "{desc}" }
+                }
+            }
+        }
+    }
+}
